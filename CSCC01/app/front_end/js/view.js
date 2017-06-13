1bc717c817956667e3f47f51e2e5871420845ba0
Vickershhh <haitao.zhu@mail.utoronto.ca>
2017-06-13 16:13:10 -0400

    Add files via upload

---
+++ CSCC01/app/front_end/js/view.js
@@ -0,0 +1,10 @@
+var view = (function(){
+    "use strict";
+	
+    var LoginForm = document.getElementById("upload_pic").addEventListener("click", Login);
+    function Login() {
+	var
+}
+    return view;
+    
+}());
